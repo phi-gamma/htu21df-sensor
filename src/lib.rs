833d4c4abedbe5aa365104907a2d2ab9707184be
Philipp Gesang <phg@phi-gamma.net>
2023-10-23 21:45:19 +0200

    implement basic commands, just sensor reset for now

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-#![no_std]
+#![allow(unused)]
+#![cfg_attr(not(test), no_std)]
 
 /*!
  * HTU21D(F) temperature / humidity sensor by TE MEAS as found in
@@ -7,42 +8,79 @@
  * Datasheet: https://cdn-shop.adafruit.com/datasheets/1899_HTU21D.pdf
  */
 
-mod constants {
+use embedded_hal::blocking::{delay::DelayUs,
+                             i2c::{Read, Write}};
+
+mod constants
+{
     /*
      * Below values are from the datasheet.
      */
     /** slave address of the HTU21D sensor */
-    const I2C_ADDR        : u8 = 0x40;
+    pub const I2C_ADDR: u8 = 0x40;
 
     /** HTU21D no-hold temperature register */
-    const I2C_TEMP        : u8 = 0xf3;
+    pub const I2C_TEMP: u8 = 0xf3;
 
     /** HTU21D no-hold humidity register */
-    const I2C_HUMI        : u8 = 0xf5;
+    pub const I2C_HUMI: u8 = 0xf5;
 
     /** power cycle the sensor */
-    const SOFT_RESET      : u8 = 0xfe;
+    pub static SOFT_RESET: [u8; 1] = [0xfe];
 
     /** wait before reading temperature */
-    const TEMP_DELAY_MS   : u8 = 50;
+    pub const TEMP_DELAY_MS: u8 = 50;
 
     /** wait before reading humidity */
-    const HUMI_DELAY_MS   : u8 = 16;
+    pub const HUMI_DELAY_MS: u8 = 16;
 
-    /** wait after resetting the sensor */
-    const RESET_DELAY_MS  : u8 = 15;
+    /**
+     * Wait after resetting the sensor (power cycle and reinitialization).
+     * It is guaranteed that the sensor reboots in less than 15 ms; cf. p.
+     * 12 of HTU21 datasheet.
+     */
+    pub const RESET_DELAY_MS: u16 = 15;
 
     /** wait for sensor to become ready */
-    const POWERUP_DELAY_MS: u8 = 15;
+    pub const POWERUP_DELAY_MS: u8 = 15;
 }
 
-#[derive(Debug, Default)]
-pub struct Sensor<I2C> {
+#[derive(Debug)]
+pub struct Sensor<I2C>
+{
     i2c: I2C,
 }
 
-impl<I2C> Sensor<I2C> {
-    pub fn create(i2c: I2C) -> Self {
-        Self { i2c }
+#[derive(Debug)]
+enum Command
+{
+    Reset,
+}
+
+impl Command
+{
+    fn as_bytes(&self) -> &'static [u8]
+    {
+        match self {
+            Command::Reset => &constants::SOFT_RESET,
+        }
+    }
+}
+
+impl<I2C, E> Sensor<I2C>
+where I2C: Read<Error = E> + Write<Error = E>
+{
+    pub fn create(i2c: I2C) -> Self { Self { i2c } }
+
+    fn send_command(&mut self, cmd: Command) -> Result<(), E>
+    {
+        self.i2c.write(constants::I2C_ADDR, cmd.as_bytes())
+    }
+
+    pub fn reset(&mut self, delay: &mut impl DelayUs<u16>) -> Result<(), E>
+    {
+        self.send_command(Command::Reset)?;
+        delay.delay_us(constants::RESET_DELAY_MS * 1000);
+        Ok(())
     }
 }
